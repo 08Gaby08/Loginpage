e0cf3d3cc156804d4d45c6fe723b378d5f41ce15
08Gaby08 <168125682+08Gaby08@users.noreply.github.com>
2024-07-02 21:08:39 -0300

    Mudei o bem vindo

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 function App() {
   return (
     <div className="app-container">
-      <h1>Bem-vindo à aplicação!</h1>
+      <h1>Bem-vindo!</h1>
       <Link to="/login">
         <button>Ir para Login</button>
       </Link>
