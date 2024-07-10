229db69e0ea510ea0b965b4345179a90c9cd3328
08Gaby08 <168125682+08Gaby08@users.noreply.github.com>
2024-07-10 16:26:42 -0300

    css

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Global } from '@emotion/react';
 import AppRouter from './router';
-import { globalStyles } from '../styles/styles';
+import { globalStyles } from './styles/styles';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
